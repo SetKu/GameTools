d01ca714b4f8e190bdedb0a2aa620c94d0479713
SetKu <78518056+SetKu@users.noreply.github.com>
2022-08-07 18:55:45 -0600

    Initial Commit

---
+++ Game Tools/Game_ToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_ToolsApp.swift
+//  Game Tools
+//
+//  Created by Zachary Morden on 2022-08-07.
+//
+
+import SwiftUI
+
+@main
+struct Game_ToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
